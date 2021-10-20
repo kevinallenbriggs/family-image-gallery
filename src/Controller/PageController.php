c1cf5973edae3b3ecb88684d579255fce23bf0b1
Kevin Briggs <kevinb@psaudio.com>
2021-10-20 00:12:58 -0600

    Updates to make old template compatible with Symfony/Twig

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -11,6 +11,27 @@ class PageController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        return $this->render('pages/home.html.twig');
+        $dir_contents = scandir('images', SCANDIR_SORT_NONE);
+
+        // filter out everything except images
+        $images = array_filter($dir_contents, function ($filename) {
+            $file_extension = pathinfo('images/' . $filename, PATHINFO_EXTENSION);
+
+            return in_array(strtolower($file_extension), ['jpeg', 'jpg', 'png', 'gif']);
+        });
+
+        $columns = [];
+
+        // sort the images into roughly equal columns
+        $current_column = 0;
+        foreach ($images as $filename) {
+            $current_column = $current_column > 2 ? 1 : $current_column;
+
+            $columns[$current_column][] = $filename;
+
+            $current_column++;
+        }
+
+        return $this->render('pages/home.html.twig', ['columns' => $columns]);
     }
 }
